1c427e7ae556b12ce77adcf11e75d4d6dbad961e
KeelyRyan <keelyannryan@gmail.com>
2024-02-21 13:37:55 +0000

    Create login functionality

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ let x  = 1;
 const postsList  = [];
 const data = {
   posts: postsList  };
+const userName = "Admin";
+const password = "passwordAdmin";
+const attemptName = "";
+const attemptPassword = "";
 
 app.use(express.static("public"));
 
@@ -15,7 +19,26 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
   const data = {
     posts: postsList  };
+    if (attemptName && attemptPassword){
+      res.render("member.ejs", data);
+    }else{
     res.render("index.ejs", data);
+    }
+});
+
+app.post("/login", (req, res) => {
+  try{
+  const attemptName = (req.body["loginName"]);
+  const attemptPassword = (req.body["loginPassword"]);
+  console.log(attemptName, attemptPassword);
+  if(attemptName == userName && attemptPassword == password){
+    res.render("member.ejs", data);
+  }else {
+    res.send("<div align ='center'><h2>Invalid email or password</h2></div><br><br><div align ='center'><a href='/'>login again</a></div>");
+  }
+}catch{
+  res.send("Internal server error");
+}
 });
 
 app.get("/about", (req, res) => {
@@ -28,6 +51,7 @@ app.get("/newPost", (req, res) => {
 
 
 app.post("/submit", (req, res) => {
+  try{
   const userName = (req.body["inputName"]);
   const userEmail = (req.body["inputEmail"]);
   const newTitle = (req.body["inputTitle"]);
@@ -37,20 +61,32 @@ app.post("/submit", (req, res) => {
 
   x ++; 
   res.render("postConfirm.ejs", addNewPost);
+}catch{
+  res.send("Internal server error please refresh the page.");
+}
 });
 
 app.get("/newPost/:id", (req, res) => {
   res.render("newPost.ejs");
+  try{
   var postId = parseInt(req.params.id);
   var post = postsList.find(post => post.postId === postId);
   post.userName = (req.body["inputName"]);
   post.userEmail = (req.body["inputEmail"]);
   post.newTitle = (req.body["inputTitle"]);
   post.newPost = (req.body["inputPost"]);
-
+}catch{
+  res.send("Internal server error please refresh the page.");
+}
 });
 
-app.get("/post/:id", (req, res) => {
+app.get("/updatePost/:id", (req, res) => {
+  var postId = parseInt(req.params.id);
+  var post = postsList.find(post => post.postId === postId);
+  res.render("readPost.ejs", {post});
+});
+
+app.get("/readPost/:id", (req, res) => {
   var postId = parseInt(req.params.id);
   var post = postsList.find(post => post.postId === postId);
   res.render("readPost.ejs", {post});
@@ -71,7 +107,11 @@ app.get("/delete/:id", (req, res) => {
   
   var postId = parseInt(req.params.id);
   var post = postsList.pop(post => post.postId === postId);
+  if (attemptName && attemptPassword){
+    res.render("member.ejs", data);
+  }else{
   res.render("index.ejs", data);
+  }
 });
 
 app.listen(port, () => {
